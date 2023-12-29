f7ee2759d4c871b424c6b33018b914db0190269c
Tony Cavella <9434191+acavella@users.noreply.github.com>
2023-12-29 23:57:21 +0000

    update main.go add viper

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/spf13/viper"
 )
+
+func init() {
+
+	viper.SetConfigName("config")        // name of config file (without extension)
+	viper.SetConfigType("yaml")          // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/appname/") // path to look for the config file in
+	viper.AddConfigPath("./conf/")       // optionally look for config in the working directory
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+}
 
 func main() {
 
 	fileUrl := "http://crls.pki.goog/gts1c3/zdATt0Ex_Fk.crl"
 	savloc := "./crl/x21.crl"
+	ca := viper.GetString("ca[0].id")
 
 	// Download the file, params:
 	// 1) name of file to save as
