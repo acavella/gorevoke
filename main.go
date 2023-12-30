34ae9e4f6730a9199786d3302eacb400468b8e5a
Tony Cavella <9434191+acavella@users.noreply.github.com>
2023-12-30 00:47:39 +0000

    update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	fileUrl := "http://crls.pki.goog/gts1c3/zdATt0Ex_Fk.crl"
 	savloc := "./crl/x21.crl"
 
-	ca := viper.GetStringSlice("ca")
+	ca := viper.GetStringSlice("ca.id")
 	fmt.Println(ca)
 
 	// Download the file, params:
