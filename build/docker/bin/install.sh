e62bbf87ffd26ae4ffdf74be2b22f405931432df
Tony Cavella <9434191+acavella@users.noreply.github.com>
2024-01-05 00:54:33 +0000

    update Docker build Dockerfile clean up builder Dockerfile  clean up runner move install script under docker

---
+++ build/docker/bin/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/evn sh
+
+set -e
+
+# Set build variables
+APP_NAME="gorevoke"
+APP_VERSION=${GOREVOKE_TAG}
+APP_BUILD="Docker"
+APP_BUILDTIME=$(date +%Y%m%d-%M%H)
+BUILD_DIR="/usr/local/bin/gorevoke"
+GOOS=linux
+GOARCH=amd64
+
+# Create application base directories
+mkdir ${BUILD_DIR}
+mkdir ${BUILD_DIR}/conf
+mkdir ${BUILD_DIR}/crl
+mkdir ${BUILD_DIR}/crl/tmp
+mkdir ${BUILD_DIR}/crl/static
+
+# Build Go application
+go build -o "${BUILD_DIR}/${APP_NAME}" -ldflags="-X 'main.appVersion=${APP_VERSION}' -X 'main.appBuild=${APP_BUILD}' -X 'main.appBuildDate=${APP_BUILDTIME}'" main.go
